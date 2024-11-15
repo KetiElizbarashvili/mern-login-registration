c4858457f7fdd12af8e075f54d4a63228403460f
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2024-11-15 12:27:41 +0400

    finished

--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -15,7 +15,9 @@ function Login() {
         axios.post('http://localhost:3001/login', { email, password })
             .then(result => {
                 console.log(result)
-                navigate('./home')
+                if (result.data === "Success") {
+                    navigate('./home')
+                }
             })
             .catch(err => console.log(err))
     }
@@ -23,7 +25,7 @@ function Login() {
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
-                <h2>Register</h2>
+                <h2>Login</h2>
                 <form onSubmit={handleSubmit}>
 
                     <div className="mb-3">
@@ -55,8 +57,20 @@ function Login() {
                         />
                     </div>
 
-                </form>
+                  
+
+
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Register
+                    </button>
+                    </form> 
+                    <p>Already have an account?</p>
+                    <Link to="/home" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                        Login
+                    </Link>                
             </div>
         </div>
     )
 }
+
+export default Login
--- server/index.js
+++ server/index.js
@@ -9,10 +9,28 @@ app.use(cors())
 
 mongoose.connect("mongodb://localhost:27017/employee");
 
-app.post('/register', (req,res) => {
-EmployeeModel.create(req.body)
-.then(employees => res.json(employees))
-.catch(err => res.json(err))
+app.post("login", (req, res) => {
+    const { email, password } = req.body;
+    EmployeeModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === pawwsord) {
+                    res.json("Success")
+                } else {
+                    res.json("password is incorrect")
+                }
+            }else{ 
+                res.json("No record Exist")
+            }
+            
+        })
+})
+
+
+app.post('/register', (req, res) => {
+    EmployeeModel.create(req.body)
+        .then(employees => res.json(employees))
+        .catch(err => res.json(err))
 })
 
 app.listen(3001, () => {
